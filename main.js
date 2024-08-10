6061135e1c05a95c2b2bc1fc0c29e2df3132eda4
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2024-08-10 18:48:12 +0800

    Finished Music Player Using HTML CSS And JavaScript

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
-
 const progress = document.getElementById("progress");
 const song = document.getElementById("song");
 const ctrlIcon = document.getElementById("ctrlIcon");
 
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+function playPause() {
+  if (ctrlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+  } else {
+    song.play();
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+  }
+}
+
+song
+  .play()
+  .then(() => {
+    setInterval(() => {
+      progress.value = song.currentTime;
+    }, 500);
+  })
+  .catch((error) => {
+    console.error("Playback failed!", error);
+  });
+
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  ctrlIcon.classList.add("fa-pause");
+  ctrlIcon.classList.remove("fa-play");
+};
